2f02424a23af741ecaeb91915ae633e8988ff81f
Moamen1928 <Moamenmohammed79@gmaol.com>
2025-07-22 14:29:17 +0300

    Update

--- javascript/script.js
+++ javascript/script.js
@@ -1,57 +1,36 @@
-const menuicon = document.getElementById("menu-icon");
-const menuLinks = document.getElementById("menu");
-
+const menuicon = document.getElementById("menu-icon"),
+  menuLinks = document.getElementById("menu");
 menuicon.addEventListener("click", () => {
-    menuLinks.classList.toggle("show");
+  menuLinks.classList.toggle("show");
 });
-// scroll to top button
 const scrollBtn = document.getElementById("up");
-window.onscroll = () => {
-    if (window.scrollY > 500) {
-        scrollBtn.classList.add("scroll");
-    } else {
-        scrollBtn.classList.remove("scroll");
-    }
-};
-scrollBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
-// navbar scroll animation
+(window.onscroll = () => {
+  window.scrollY > 500
+    ? scrollBtn.classList.add("scroll")
+    : scrollBtn.classList.remove("scroll");
+}),
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
 const navbarItems = document.querySelectorAll(".nav li");
-
-navbarItems.forEach((item) => {
-    item.addEventListener("click", () => {
-    navbarItems.forEach((el) => el.classList.remove("active"));
-    
-    item.classList.add("active");
-    });
+navbarItems.forEach((e) => {
+  e.addEventListener("click", () => {
+    navbarItems.forEach((e) => e.classList.remove("active")),
+      e.classList.add("active");
+  });
 });
-
-
-// Reveal on Scroll
 const revealElements = document.querySelectorAll(".reveal");
-
 window.addEventListener("scroll", () => {
-revealElements.forEach((el, index) => {
-    const windowHeight = window.innerHeight;
-    const elementTop = el.getBoundingClientRect().top;
-    const elementVisible = 100;
-
-    if (elementTop < windowHeight - elementVisible) {
-    setTimeout(() => {
-        el.classList.add("active");
-      }, index * 200);
-    } else {
-    el.classList.remove("active");
-    }
-    });
-});
-
-window.addEventListener("load", () => {
+  revealElements.forEach((e, t) => {
+    let l = window.innerHeight,
+      s = e.getBoundingClientRect().top;
+    s < l - 100
+      ? setTimeout(() => {
+          e.classList.add("active");
+        }, 200 * t)
+      : e.classList.remove("active");
+  });
+}),
+  window.addEventListener("load", () => {
     window.dispatchEvent(new Event("scroll"));
-});
-
-
+  });
